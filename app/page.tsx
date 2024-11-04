7e81075bd3374fc3f9165b12fd1bf3fb83f57982
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2024-11-04 17:50:43 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
   };
 
   useEffect(() => {
-    redirectToApp(); // Run the function on page load
+    // redirectToApp(); // Run the function on page load
   }, []);
 
   return (
