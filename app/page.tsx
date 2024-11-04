86e26e614c7349ad3325e98842ae96d52481577a
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2024-11-04 17:02:34 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect } from "react";
 import Image from "next/image";
 
@@ -33,7 +34,7 @@ export default function Home() {
   useEffect(() => {
     redirectToApp(); // Run the function on page load
   }, []);
-  
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
