5954a6eb20fba4c4c8b2c50113884d3fcc63a6e9
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2024-11-04 11:35:28 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        destination: '/.well-known/apple-app-site-association',
+      },
+    ]
+  },
   /* config options here */
 };
 
