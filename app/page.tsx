d048378ac9213d16d3911ebeb46eb0dee1f73ac4
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2025-02-17 15:47:48 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 "use client"
-import { useState } from "react";
-import Link from 'next/link';
 
 export default function Home() {
-
-  
 
   // Helper function for device detection
   const isiOS = () => /iPhone|iPad|iPod/i.test(navigator.userAgent);
