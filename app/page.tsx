bab0c22271d77267b9ea6c5710e78c871fa7e332
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2025-02-17 15:37:11 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -80,66 +80,45 @@ export default function Home() {
 
   const openAndSaveCard = (card_id:any,card_owner_id:any) =>{
 
-    let fallbackLink = '';
+    try {
 
-    // isiOS isAndroid implement by yourself
-    if (isiOS() || isAndroid()) {
-      // const card_id = "677e036bee9675ee44b3dc65"
-      // const card_owner_id = "677e01aeee9675ee44b3dc43"
+      let fallbackLink = '';
 
-      const card_for_saved = {
-        "cardId":card_id,
-        "ownerId":card_owner_id
+      // isiOS isAndroid implement by yourself
+      if (isiOS() || isAndroid()) {
+        // const card_id = "677e036bee9675ee44b3dc65"
+        // const card_owner_id = "677e01aeee9675ee44b3dc43"
+  
+        const card_for_saved = {
+          "cardId":card_id,
+          "ownerId":card_owner_id
+        }
+        console.log("=-=-card_for_saved",card_for_saved)
+        const encodedData = encodeURIComponent(JSON.stringify(card_for_saved));
+          window.location.href = `saveseecard://open?id=${encodedData}`;
+          const androidAppStoreLink = 'https://play.google.com/store/apps/details?id=com.seecard';
+          const iosAppStoreLink = 'https://apps.apple.com/np/app/seecard/id6502513661';
+          fallbackLink = isAndroid() ? androidAppStoreLink : iosAppStoreLink;
+          const timeout = setTimeout(function () {
+              if (document.hasFocus()) {
+                window.location.href = fallbackLink;
+              }
+          }, 2000);
+  
+          window.addEventListener('blur', () => {
+              clearTimeout(timeout);;
+          });
+      } else {
+        alert("Your device doesn't support deep linking for this app.");
       }
-      console.log("=-=-card_for_saved",card_for_saved)
-      const encodedData = encodeURIComponent(JSON.stringify(card_for_saved));
-        window.location.href = `saveseecard://open?id=${encodedData}`;
-        const androidAppStoreLink = 'https://play.google.com/store/apps/details?id=com.seecard';
-        const iosAppStoreLink = 'https://apps.apple.com/np/app/seecard/id6502513661';
-        fallbackLink = isAndroid() ? androidAppStoreLink : iosAppStoreLink;
-        const timeout = setTimeout(function () {
-            if (document.hasFocus()) {
-              window.location.href = fallbackLink;
-            }
-        }, 2000);
-
-        window.addEventListener('blur', () => {
-            clearTimeout(timeout);;
-        });
-    } else {
-      alert("Your device doesn't support deep linking for this app.");
+    } catch (e) {
+      // alert(JSON.stringify(e))
+      console.log("catch error print -------------------",JSON.stringify(e))
     }
+
   }
 
-//   useEffect(() => {
-//     const checkAppInstalled = () => {
-//         const appUrl = 'mychat://open';
-        
-//         // const fallbackUrl = 'https://play.google.com/store/apps/details?id=com.whatsapp';
-
-//         // Try to open the app
-//         window.location.href = appUrl;
-
-//         // If the app doesn’t open, display the download option
-//         const timeout = setTimeout(() => {
-//             setIsInstalled(false);
-//         }, 1000);
-
-//         // If the app is installed, handle the "blur" event
-//         window.addEventListener('blur', () => {
-
-//             clearTimeout(timeout);
-//             setIsInstalled(true);
-//             alert("Blur")
-//         });
-
-//         // Cleanup event listener on unmount
-//         return () => window.removeEventListener('blur', () => setIsInstalled(true));
-//     };
-
-//     checkAppInstalled();
-// }, []);
-
+  
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
