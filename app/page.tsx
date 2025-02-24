108a517e11630fc2dd45410e70417b0662b395e3
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2025-02-24 12:04:06 +0500

    wip: improving screen designs

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ const APP_STORE_URL = "https://apps.apple.com/app/idYOUR_APP_ID";
 export default function Home() {
   const [isAppInstalled, setIsAppInstalled] = useState(false);
 
-  const isAndroid = () => /Android/i.test(navigator.userAgent);
-  const isiOS = () => {
-    const userAgent = navigator.userAgent || navigator.vendor;
-    return (
-      /iPhone|iPad|iPod/.test(userAgent) || 
-      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
-    );
-  };
+  // const isAndroid = () => /Android/i.test(navigator.userAgent);
+  // const isiOS = () => {
+  //   const userAgent = navigator.userAgent || navigator.vendor;
+  //   return (
+  //     /iPhone|iPad|iPod/.test(userAgent) || 
+  //     (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+  //   );
+  // };
   
   
   useEffect(() => {
